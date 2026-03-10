bdc88eebf46299cb6237d0ef94f547e27ba36c60
brunobgss <brunobgs1888@gmail.com>
2026-03-10 08:40:42 -0300

    feat: adicionar Google Analytics e verificação do site no layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -148,6 +148,18 @@ export default function RootLayout({
   return (
     <html lang="pt-BR" className={poppins.variable}>
       <head>
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-MQ6ZJ06CXW"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-MQ6ZJ06CXW');
+            `,
+          }}
+        />
         <meta name="google-site-verification" content="FjI2LKCSn8rXl5GiQXA4bcBQ5H279tYb0qSrGDVCT5w" />
         <script
           type="application/ld+json"
